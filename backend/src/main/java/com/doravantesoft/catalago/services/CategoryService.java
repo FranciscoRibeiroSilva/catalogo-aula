be265eed5c23760de34ea809f49d8ba773292fc5
FranciscoRibeiroSilva <franciscoribeirsil@gmail.com>
2024-10-31 13:03:30 -0300

    Category DTO

--- backend/src/main/java/com/doravantesoft/catalago/services/CategoryService.java
+++ backend/src/main/java/com/doravantesoft/catalago/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.doravantesoft.catalago.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.doravantesoft.catalago.DTO.CategoryDTO;
 import com.doravantesoft.catalago.entities.Category;
 import com.doravantesoft.catalago.repositories.CategoryRepository;
 
@@ -16,8 +18,20 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true)//ou faz tudo ou não faz nada (readOnly = true)
-	public List<Category> findAll(){
-		return repository.findAll();
+	public List<CategoryDTO> findAll(){
+		List <Category> list = repository.findAll();
+		
+		//transforma a lista de categoria em uma stream, e para cada elemento da stream um novo CategoryDTO é instanciado
+		//depois usando o collerct é transforma de novo em list e retornado como uma lista de CategoyDTO
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		
+		/**
+		List <CategoryDTO> listDto = new ArrayList<>();
+		for (Category cat : list) {
+			listDto.add(new CategoryDTO(cat));
+		}
+		return listDto;
+		*/
 	}
 
 }
