70b2825a5ca4c631c076881c618fb64cedc3f861
FranciscoRibeiroSilva <franciscoribeirsil@gmail.com>
2024-10-31 12:07:58 -0300

    h2 database seeding

--- backend/src/main/java/com/doravantesoft/catalago/services/CategoryService.java
+++ backend/src/main/java/com/doravantesoft/catalago/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.doravantesoft.catalago.entities.Category;
 import com.doravantesoft.catalago.repositories.CategoryRepository;
@@ -14,6 +15,7 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
+	@Transactional(readOnly = true)//ou faz tudo ou não faz nada (readOnly = true)
 	public List<Category> findAll(){
 		return repository.findAll();
 	}
---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO 	tb_category(name) VALUES ('Books');
+INSERT INTO 	tb_category(name) VALUES ('Eletronics');
+INSERT INTO 	tb_category(name) VALUES ('Foods');
+INSERT INTO 	tb_category(name) VALUES ('Games');
+INSERT INTO 	tb_category(name) VALUES ('Toys');
