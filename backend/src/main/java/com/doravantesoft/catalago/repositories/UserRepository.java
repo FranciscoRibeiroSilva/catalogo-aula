30606786f5ecc2f4c7585a6954d1960f0980cf6c
FranciscoRibeiroSilva <franciscoribeirsil@gmail.com>
2025-01-20 13:22:18 -0300

    user crud

---
+++ backend/src/main/java/com/doravantesoft/catalago/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.doravantesoft.catalago.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.doravantesoft.catalago.entities.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
---
+++ backend/src/main/java/com/doravantesoft/catalago/services/UserService.java
@@ -0,0 +1,129 @@
+package com.doravantesoft.catalago.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.doravantesoft.catalago.DTO.RoleDTO;
+import com.doravantesoft.catalago.DTO.UserDTO;
+import com.doravantesoft.catalago.DTO.UserInsertDTO;
+import com.doravantesoft.catalago.entities.Role;
+import com.doravantesoft.catalago.entities.User;
+import com.doravantesoft.catalago.repositories.RoleRepository;
+import com.doravantesoft.catalago.repositories.UserRepository;
+import com.doravantesoft.catalago.services.exceptions.DatabaseException;
+import com.doravantesoft.catalago.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+	
+	@Autowired
+	private UserRepository repository;
+	
+	@Autowired
+	private RoleRepository roleRepository;
+	
+	@Transactional(readOnly = true)//ou faz tudo ou não faz nada (readOnly = true)
+	public Page<UserDTO> findAllPaged(Pageable pageable){
+		Page <User> list = repository.findAll(pageable);
+		
+		return list.map(x -> new UserDTO(x));
+		
+		//transforma a lista de categoria em uma stream, e para cada elemento da stream um novo CategoryDTO é instanciado
+		//depois usando o collerct é transforma de novo em list e retornado como uma lista de CategoyDTO
+		//return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		
+		/**
+		List <CategoryDTO> listDto = new ArrayList<>();
+		for (Category cat : list) {
+			listDto.add(new CategoryDTO(cat));
+		}
+		return listDto;
+		*/
+	}
+		
+	/**
+	 *Optional evita o trabalho com valores nulos
+	 */
+	@Transactional(readOnly = true)
+	public UserDTO findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		User entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+		return new UserDTO(entity);
+	}
+	
+	@Transactional
+	public UserDTO insert(UserInsertDTO dto) {
+		User entity = new User();
+		copyDtoToEntity(dto, entity);
+		entity.setPassword(passwordEncoder.encode(dto.getPassword()));
+		entity = repository.save(entity);
+		return new UserDTO(entity);
+	}
+	
+	@Transactional 
+	public UserDTO update(Long id, UserDTO dto) {
+		try {
+			User entity = repository.getReferenceById(id);
+			copyDtoToEntity(dto, entity);
+			entity = repository.save(entity);
+			return new UserDTO(entity);			
+		}
+		catch(EntityNotFoundException e){
+			throw new ResourceNotFoundException("id not found "+id);
+		}
+	}
+
+	//@Transactional(propagation = Propagation.SUPPORTS)
+	public void delete(Long id) {
+		if(!repository.existsById(id)) {
+			throw new ResourceNotFoundException("id resource not found");
+		}
+		try {
+			repository.deleteById(id);
+		}
+		catch(DataIntegrityViolationException e){
+			throw new DatabaseException("integrity violation");
+		}
+		
+	}
+	
+	private void copyDtoToEntity(UserDTO dto, User entity) {
+		entity.setFirstName(dto.getFirstName());
+		entity.setLastName(dto.getLastName());
+		entity.setEmail(dto.getEmail());
+		
+		entity.getRoles().clear();
+		for(RoleDTO roleDTO: dto.getRoles()) {
+			Role role = roleRepository.getReferenceById(roleDTO.getId());
+			entity.getRoles().add(role);
+			//entity.getCategories().add(new Category (catDto.getId(), null));
+		}
+	}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
